d99bfa05cdf27032d3917eb2a023743c705938d0
unknown <juslepi@gmail.com>
2023-08-20 15:39:51 +0300

    db connection

--- server.ts
+++ server.ts
@@ -4,16 +4,18 @@ import mongoose from "mongoose";
 import { MONGO_URI, PORT } from "./config/config";
 
 // Connect to the database
-try {
-  mongoose.connect(MONGO_URI ?? "");
-  console.log("DB Connected");
-} catch (error) {
-  console.error(error);
-}
+(async function () {
+  try {
+    await mongoose.connect(MONGO_URI ?? "");
+    console.log("DB Connected");
+  } catch (error) {
+    console.error(error);
+  }
+})();
 
 const app = express();
 app.use("/api/journeys", journeyRouter);
 
 app.listen(PORT, () => {
-  console.log("Server online");
+  console.log("Server online on", PORT);
 });
